a1d271b85162c4251a523080bf7be05f50174cfb
Pujan Bohora <bohorapawan@gmail.com>
2024-07-08 09:33:17 +0545

    save data to local

--- coreDateAddData/ContentView.swift
+++ coreDateAddData/ContentView.swift
@@ -12,40 +12,84 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \ProgrammingLanguage.creater, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var items: FetchedResults<ProgrammingLanguage>
+    
+    @State private var showAlert = false
+    @State private var creator = ""
+    @State private var name = ""
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        
+        
+//        NavigationView {
+//            List {
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+//                    } label: {
+//                        Text(item.timestamp!, formatter: itemFormatter)
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+//                ToolbarItem {
+//                    Button(action: addItem) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//            Text("Select an item")
+//        }
+        
+                NavigationView {
+                    List {
+                        ForEach(items) { item in
+                            NavigationLink {
+                                Text("Item at \(item.creater!)")
+                            } label: {
+                                Text(item.name!)
+                            }
+                        }
+//                        .onDelete(perform: deleteItems)
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+
+                    .toolbar {
+                      ToolbarItem {
+                        Button {
+                          showAlert = true
+                        } label: {
+                          Label("Add Item", systemImage: "plus")
+                        }
+                      }
                     }
+                    .alert("Login", isPresented: $showAlert) {
+                        VStack {
+                                TextField("Creator", text: $creator)
+                                .padding(.bottom, 10)
+                                
+                            TextField("Name", text: $name)
+                            Button("Save") {
+                                addItem()
+                                        }
+                            }
+                        .padding()
+                            
+                    }
+                    Text("Select an item")
                 }
-            }
-            Text("Select an item")
-        }
     }
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newItem = ProgrammingLanguage(context: viewContext)
+            newItem.creater = creator
+            newItem.name = name
 
             do {
                 try viewContext.save()
@@ -58,20 +102,20 @@ struct ContentView: View {
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            offsets.map { items[$0] }.forEach(viewContext.delete)
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
 }
 
 private let itemFormatter: DateFormatter = {
--- coreDateAddData/Persistence.swift
+++ coreDateAddData/Persistence.swift
@@ -7,16 +7,57 @@
 
 import CoreData
 
+//struct PersistenceController {
+//    // A singleton for our entire app to use
+//    static let shared = PersistenceController()
+//
+//    // Storage for Core Data
+//    let container: NSPersistentContainer
+//
+//    // A test configuration for SwiftUI previews
+//    static var preview: PersistenceController = {
+//        let controller = PersistenceController(inMemory: true)
+//
+//        // Create 10 example programming languages.
+//        for _ in 0..<10 {
+//            let language = ProgrammingLanguage(context: controller.container.viewContext)
+//            language.name = "Example Language 1"
+//            language.creater = "A. Programmer"
+//        }
+//
+//        return controller
+//    }()
+//
+//    // An initializer to load Core Data, optionally able
+//    // to use an in-memory store.
+//    init(inMemory: Bool = false) {
+//        // If you didn't name your model Main you'll need
+//        // to change this name below.
+//        container = NSPersistentContainer(name: "Main")
+//
+//        if inMemory {
+//            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+//        }
+//
+//        container.loadPersistentStores { description, error in
+//            if let error = error {
+//                fatalError("Error: \(error.localizedDescription)")
+//            }
+//        }
+//    }
+//}
+
 struct PersistenceController {
     static let shared = PersistenceController()
 
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
+//        for _ in 0..<10 {
+            let newItem = ProgrammingLanguage(context: viewContext)
+            newItem.creater = "Puja"
+            newItem.name = "Test"
+//        }
         do {
             try viewContext.save()
         } catch {
@@ -54,3 +95,5 @@ struct PersistenceController {
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
+
+
